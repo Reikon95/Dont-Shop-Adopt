55d8e608f30e5ccddf6e1abc08f38cd15537f477
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-15 11:40:33 +0000

    added all conditionals

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Explanation from './Explanation.js'
 
 function App() {
-  const dogs = [{id: 'bc01', breed: 'Border Collie', size: 7, activityLevel: 10, officeDog: false, health: 10}, {id: 'p01', breed: 'Pug',
-  size: 3, activityLevel: 2, officeDog: true, health: 2}, {id: 'si01', breed: 'Shiba Inu', size: 5, activityLevel: 7, 
-  officeDog: true, health: 10}]
+  const dogs = [{id: 'bc01', breed: 'Border Collie', size: 7, activityLevel: 10, officeDog: false, health: true}, {id: 'p01', breed: 'Pug',
+  size: 3, activityLevel: 2, officeDog: true, health: false}, {id: 'si01', breed: 'Shiba Inu', size: 5, activityLevel: 7, 
+  officeDog: true, health: true}]
   return (
     <div className="App">
       It seems our generation loves dogs a lot. However, many people don't realise how many great rescue dogs are available to adopt. Answer some questions to find your perfect pet!
-      <Explanation {...dogs[1]}/>
+      <Explanation {...dogs[0]}/>
     </div>
   );
 }
--- src/Explanation.js
+++ src/Explanation.js
@@ -5,7 +5,12 @@ function Explanation(props) {
         <>
         <h1>You should consider a {props.breed}!</h1>
         <>{(props.size < 5) ? <p>It's a smaller dog</p> : <p>It's a larger dog</p>}</>
-        <>{(props.activityLevel < 5) ? <p>a {props.breed} doesn't require too much exercise</p> : <p>{props.breed} is great to exercise alongside!</p>}</>
+        <>{(props.activityLevel < 5) ? <p>a {props.breed} doesn't require too much exercise</p> : 
+                <p>{props.breed}s are great to exercise alongside!</p>}</>
+        <>{props.officeDog ? <p>You can take the {props.breed} into work with you and shouldn't have issues</p> :
+                <p>It probably isn't a good idea to take your {props.breed} to work on a regular basis</p>}</>
+        <>{props.health ? <p>The {props.breed} is typically a healthy dog, so you needn't worry about the ethics or vet bills</p> :
+                <p>You should consider that {props.breed} does have some health issues. Maybe you could consider a crossbreed?</p>}</>
         </>
         )
     
