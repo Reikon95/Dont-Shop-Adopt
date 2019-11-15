b4d1a0ea9847765ad55b521334a1fde3842f8956
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-15 10:04:43 +0000

    component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Explanation from './Explanation.js'
 
 function App() {
+  const dogs = [{id: 'bc01', breed: 'Border Collie', size: 7, activityLevel: 10, officeDog: false, health: 10}, {id: 'p01', breed: 'Pug',
+  size: 3, activityLevel: 2, officeDog: true, health: 2}, {id: 'si01', breed: 'Shiba Inu', size: 5, activityLevel: 7, 
+  officeDog: true, health: 10}]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Explanation dogs[0]/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ src/Explanation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Explanation(props){
+     
+        return
+        <>
+        <h1>You should consider a {props.breed}!</h1>
+        <p>Because xyz</p>
+        </>
+    
+}
+
+export default Explanation
