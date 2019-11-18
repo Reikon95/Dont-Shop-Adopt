6d409d0ee4cae60908d71a9b57eaca9787c4e583
Cameron Blackwood <cameronjwblackwood@gmail.com>
2019-11-18 12:57:48 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import Intro from './Intro.js'
 function App() {
   return (
     <div className="App">
+      <div className="navbar">
+        <ul>
+          <li>Why rescue a dog</li>
+          <a href="https://portfolio.cameronjwblackwood.now.sh/" target="_blank" rel="noopener noreferrer"><li>More about me</li></a>        
+        </ul>
+      </div>
       <div className="intro">
         <Intro/>
       </div>
